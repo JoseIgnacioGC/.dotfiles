192d4295859a04e1e011b630887b7ef570596284
Jose Leiva <email.joseleiva@gmail.com>
2023-11-17 18:01:53 -0300

    Add lazyvim symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,4 +1,5 @@
 # symlinks
-ln -fsv $DOTFILES/zsh/{.zshrc,.zprofile,.zsh_logout} $HOME/ 
+ln -fsv $DOTFILES/zsh/{.zshrc,.zprofile,.zsh_logout} $HOME/
 ln -fsv $DOTFILES/tmux/.tmux.conf $HOME/
 ln -fsv $DOTFILES/nvim/nvchad/custom $HOME/.config/nvim/lua/
+ln -fsv $DOTFILES/nvim/lazyvim $HOME/.config/
