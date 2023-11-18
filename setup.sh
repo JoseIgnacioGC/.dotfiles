e93360650ddc6158e8d6c238b465c1d032eccd76
Jose Leiva <email.joseleiva@gmail.com>
2023-11-17 22:17:26 -0300

    Refactor the entire setup file

--- setup.sh
+++ setup.sh
@@ -1,40 +1,38 @@
 # local env vars
 DOTFILES="$HOME/.dotfiles"
 
-# Create basic folders
-mkdir ~/Documents ~/Downloads ~/Trash ~/Workspace
-mkdir ~/Workspace/{projects,forks,lern}
-mkdir -p ~/bin ~/.local/bin
+# create folders
+mkdir $HOME/{Documents,Downloads,Trash}
+mkdir -p $HOME/Workspace/{projects,forks,lern}
+mkdir -p $HOME/bin $HOME/.local/bin
 
 # ssh key - generate
-ssh-keygen -o -f ~/.ssh/id_rsa
+ssh-keygen -o -f $HOME/.ssh/id_rsa
 
 # apt - install packages
 sudo apt update && sudo apt upgrade
-sudo apt -y install curl
-sudo apt -y install build-essential
+sudo apt -y install curl wget
+sudo apt -y install build-essential unzip
 sudo apt -y install man-db manpages-dev
-sudo apt -y install fzf
-sudo apt -y install zoxide
-sudo apt -y install ripgrep
-sudo apt -y install unzip
-sudo apt -y install wget
+sudo apt -y install fzf ripgrep fd-find
 sudo apt -y install git-all
+sudo apt -y install zoxide
 sudo apt -y install zsh
 sudo apt -y install tmux
 sudo apt -y install neovim
+# apt - symlink to prevent issues
 sudo apt -y install fd-find
-ln -vs $(which fdfind) ~/.local/bin/fd # debian-based distros - symlink to prevent issues
+ln -vs $(which fdfind) $HOME/.local/bin/fd # only debian-based distros
 sudo apt -y install bat
-ln -vs $(which batcat) ~/.local/bin/bat # debian-based distros - symlink to prevent issues
+ln -vs $(which batcat) $HOME/.local/bin/bat # only debian-based distros
 # apt - install pip packages
 sudo apt -y install python3.11-venv
 sudo apt -y install python3-pip
 sudo apt -y install python3-mercurial
 
-# TODO: git - setup submodules
+# git - setup submodules
 git submodule init
-git submoduel update
+git submodule update
 
 # pip - install packages
 pip install mercurial
@@ -62,11 +60,11 @@ npm i -g neovim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # nvim - setup NvChad
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+git clone https://github.com/NvChad/NvChad $HOME/.config/nvim --depth 1 && nvim
 
 # tmux - setup plugin manager
-gh repo clone tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux source ~/.tmux.conf # TODO: "tmux readme - Press prefix + I (capital i, as in Install) to fetch the plugin."
+gh repo clone tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+tmux source $HOME/.tmux.conf # TODO: "tmux readme - Press prefix + I (capital i, as in Install) to fetch the plugin."
 # tmux - as default terminal
 chsh -s $(which tmux)
 
