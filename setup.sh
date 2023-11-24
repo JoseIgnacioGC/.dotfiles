b13980cdf5d99b5689a2ca09d34ea6295d709fc6
Jose Leiva <email.joseleiva@gmail.com>
2023-11-23 22:11:08 -0300

    Remove smart tmux session manager plugin

--- setup.sh
+++ setup.sh
@@ -15,7 +15,6 @@ sudo apt -y install curl wget
 sudo apt -y install build-essential unzip
 sudo apt -y install man-db manpages-dev
 sudo apt -y install fzf ripgrep fd-find
-sudo apt -y install zoxide
 sudo apt -y install zsh
 sudo apt -y install tmux
 sudo apt -y install neovim
