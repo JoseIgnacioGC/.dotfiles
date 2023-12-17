bccd1931e0ce0efcd09f234c7523e13df1efe6a5
Jose Leiva <email.joseleiva@gmail.com>
2023-12-17 00:18:52 -0300

    Add windows hello integration for sudo

--- wsl_only.sh
+++ wsl_only.sh
@@ -1,4 +1,12 @@
-# xdg-open
+# xdg-open wsl compatibility
 curl -o xdg-open https://github.com/cpbotha/xdg-open-wsl/blob/master/xdg_open_wsl/xdg_open_wsl.py
 mv -vf xdg-open $HOME/.local/bin/
 ln -fsv $HOME/.local/bin/xdg-open $HOME/.local/bin/open
+
+# windows hello PAM module for sudo
+wget http://github.com/nullpo-head/WSL-Hello-sudo/releases/latest/download/release.tar.gz
+tar xvf release.tar.gz
+cd release
+./install.sh
+cd ..
+rm -fr release.tar.gz release
