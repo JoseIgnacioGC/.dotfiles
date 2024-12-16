f7eee7d58e626f4468c75452daad25f007a245b4
Jose Leiva <email.joseleiva@gmail.com>
2024-12-16 00:02:22 -0300

    refactor: config files syntax

--- shell/symlinks.sh
+++ shell/symlinks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ln -fsv "${DOTFILES}/git/.gitconfig" "${HOME}"
-ln -fsv "${DOTFILES}"/zsh/{.zshrc,.zprofile,.zsh_logout} "${HOME}"
-ln -fsv "${DOTFILES}"/tmux/.tmux.conf "${HOME}"
+ln -fsv "${DOTFILES}/zsh/{.zshrc,.zprofile,.zsh_logout}" "${HOME}"
+ln -fsv "${DOTFILES}/tmux/.tmux.conf" "${HOME}"
+ln -fsv "${DOTFILES}/helix/config.toml" "${HOME}/.config/helix"
